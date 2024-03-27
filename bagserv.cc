cc6ad9ad35dc26b6ba6aaca097cfcf699ce1e7f7
bert hubert <bert@hubertnet.nl>
2024-03-27 12:03:52 +0100

    add postcode to geo lookup, closing #11

--- bagserv.cc
+++ bagserv.cc
@@ -39,8 +39,6 @@ int main(int argc, char**argv)
   };
   LockedSqw lsqw{sqw, sqwlock};
 
-  // SQW IS NOT IN ANY WAY THREAD SAFE SO DO NOT FORGET THE LOCK!!
-
   svr.Get(R"(/(\d\d\d\d[A-Z][A-Z])/(\d+)/([a-zA-Z]))", [&lsqw](const httplib::Request &req, httplib::Response &res) {
     auto pcode = req.matches[1];
     auto huisnum = req.matches[2];
@@ -86,9 +84,24 @@ int main(int argc, char**argv)
     //      cout<<"lat: "<<lat<<endl;
     // cout<<"lon: "<<lon<<endl;
     cout<<"Query for "<<lat<<", "<<lon<<endl;
-    lsqw.queryJ(res, "select x as rdX, y as rdY,straat,woonplaats,huisnummer,huisletter,huistoevoeging,oppervlakte,bouwjaar,lon,lat,gebruiksdoelen,num_status,vbo_status, (lat-?)*(lat-?)+(lon-?)*(lon-?) as deg2dist from geoindex,alllabel where alllabel.vbo_id = geoindex.vbo_id and minLat > ? and maxLat < ? and minLon > ? and maxLon < ? order by deg2dist asc limit 1", {lat, lat, lon, lon, lat-0.005, lat+0.005, lon-0.005, lon+0.005}) ;
+    lsqw.queryJ(res, "select x as rdX, y as rdY,straat,woonplaats,huisnummer,huisletter,huistoevoeging,postcode,oppervlakte,bouwjaar,lon,lat,gebruiksdoelen,num_status,vbo_status, (lat-?)*(lat-?)+(lon-?)*(lon-?) as deg2dist from geoindex,alllabel where alllabel.vbo_id = geoindex.vbo_id and minLat > ? and maxLat < ? and minLon > ? and maxLon < ? order by deg2dist asc limit 1", {lat, lat, lon, lon, lat-0.005, lat+0.005, lon-0.005, lon+0.005}) ;
   });
 
+  /*
+  svr.Get(R"(/street-partial/:street)", [&lsqw](const httplib::Request &req, httplib::Response &res) {
+    string street = req.path_params.at("street");
+    street = "%"+street+"%";
+    cout<<"Searching for '"<<street<<"'\n";
+    lsqw.queryJ(res, "select oprs.id,naam,woonplaats,count(1) as 'count' from oprs,nums where naam like ? and oprs.id==nums.ligtAanRef group by oprs.id order by 4 desc limit 10", {street});
+  });
+
+  svr.Get(R"(/street-partial2/:street)", [&lsqw](const httplib::Request &req, httplib::Response &res) {
+    string street = req.path_params.at("street");
+    cout<<"Searching for '"<<street<<"'\n";
+    lsqw.queryJ(res, "select oprs.id,naam,woonplaats,count(1) as 'count' from tri(?),oprs,nums where oprs.naam=a and oprs.id==nums.ligtAanRef group by oprs.id order by 4 desc limit 10", {street});
+  });
+  */
+  
   int port = argc==1 ? 8080 : atoi(argv[1]);
   cout<<"Will listen on http://127.0.0.1:"<<port<<endl;
   
