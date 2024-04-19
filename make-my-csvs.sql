232cc3d12399a4fcc72456437b6d313ff94c42d4
Maurits van der Schee <maurits@vdschee.nl>
2024-04-19 02:04:54 +0200

    update

--- make-my-csvs.sql
+++ make-my-csvs.sql
@@ -2,7 +2,7 @@
 .mode csv
 .output postcodes-nl-geo.csv
 
-select straat,huisnummer,huisletter,huistoevoeging,woonplaats,postcode,CASE WHEN x = -1 THEN '' ELSE CAST(round(x) as INT) END as rd_x,CASE WHEN x = -1 THEN '' ELSE CAST(round(y) as INT) END as rd_y
+select straat,huisnummer,huisletter,huistoevoeging,woonplaats,postcode,CASE WHEN x = -1 THEN 0 ELSE CAST(round(x) as INT) END as rd_x,CASE WHEN x = -1 THEN 0 ELSE CAST(round(y) as INT) END as rd_y
 from unilabel 
 where postcode!= '' 
 order by postcode,huisnummer,huisletter,huistoevoeging;
