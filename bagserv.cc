b8be1bd6d7e92bce5a55c966136a6a1132d15d0a
bert hubert <bert@hubertnet.nl>
2024-10-29 10:50:35 +0100

    up number of thread

--- bagserv.cc
+++ bagserv.cc
@@ -1,4 +1,6 @@
 #define CPPHTTPLIB_USE_POLL
+#define CPPHTTPLIB_THREAD_POOL_COUNT 256
+
 #include "httplib.h"
 #include "sqlwriter.hh"
 #include "json.hpp"
@@ -6,8 +8,25 @@
 #include <mutex>
 #include "thingpool.hh"
 #include "jsonhelper.hh"
-
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 using namespace std;
+
+struct DTime
+{
+  DTime()
+  {
+    d_start =   std::chrono::steady_clock::now();
+  }
+  uint32_t usec()
+  {
+    auto usec = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now()- d_start).count();
+    return usec;
+  }
+
+  std::chrono::time_point<std::chrono::steady_clock> d_start;
+};
+
 
 static void convToHTTP(httplib::Response & res, const std::vector<std::unordered_map<std::string,MiniSQLite::outvar_t>>& content)
 {
@@ -24,7 +43,8 @@ int main(int argc, char**argv)
 {
   httplib::Server svr;
   svr.set_mount_point("/", "./html/");
-
+  //  svr.set_keep_alive_max_count(1); // Default is 5
+  //svr.set_keep_alive_timeout(1);  // Default is 5
   ThingPool<SQLiteWriter> tp("bag.sqlite", SQLWFlag::ReadOnly);
   
   svr.Get(R"(/(\d\d\d\d[A-Z][A-Z])/(\d+)/([a-zA-Z]))", [&tp](const httplib::Request &req, httplib::Response &res) {
@@ -33,6 +53,7 @@ int main(int argc, char**argv)
     auto huisletter = (string)req.matches[3];
     if(!huisletter.empty())
       huisletter[0]=toupper(huisletter[0]);
+
     convToHTTP(res, tp.getLease()->queryT("select x as rdX, y as rdY,straat,woonplaats,huisnummer,huisletter,huistoevoeging,oppervlakte,lon,lat,gebruiksdoelen,bouwjaar,num_status,vbo_status from alllabel where postcode=? and huisnummer=? and huisletter=?", {pcode, huisnum,huisletter}));
   });
 
@@ -51,6 +72,7 @@ int main(int argc, char**argv)
   svr.Get(R"(/(\d\d\d\d[A-Z][A-Z])/(\d+))", [&tp](const httplib::Request &req, httplib::Response &res) {
     auto pcode = req.matches[1];
     auto huisnum = req.matches[2];
+
     convToHTTP(res, tp.getLease()->queryT("select x as rdX, y as rdY, straat,woonplaats,huisnummer,huisletter,huistoevoeging,oppervlakte,lon,lat,gebruiksdoelen,bouwjaar,num_status,vbo_status from alllabel where postcode=? and huisnummer=?", {pcode, huisnum}));
   });
 
@@ -62,12 +84,11 @@ int main(int argc, char**argv)
 
   // coordinates
   svr.Get(R"(/(\d*\.\d*)/(\d*\.\d*))", [&tp](const httplib::Request &req, httplib::Response &res) {
-    // 52.04233332908745, 4.386007891085206
+    // 52.04233332908745/4.386007891085206
     auto latstr = (string)req.matches[1];  // "y value, > 50"
     auto lonstr = (string)req.matches[2]; // "x value, between 3 and 7"
     double lat = atof(latstr.c_str());
     double lon = atof(lonstr.c_str());
-
     convToHTTP(res, tp.getLease()->queryT("select x as rdX, y as rdY,straat,woonplaats,huisnummer,huisletter,huistoevoeging,postcode,oppervlakte,bouwjaar,lon,lat,gebruiksdoelen,num_status,vbo_status, (lat-?)*(lat-?)+(lon-?)*(lon-?) as deg2dist from geoindex,alllabel where alllabel.vbo_id = geoindex.vbo_id and minLat > ? and maxLat < ? and minLon > ? and maxLon < ? order by deg2dist asc limit 1", {lat, lat, lon, lon, lat-0.005, lat+0.005, lon-0.005, lon+0.005})) ;
   });
 
@@ -101,6 +122,10 @@ int main(int argc, char**argv)
     lsqw.queryJ(res, "select oprs.id,naam,woonplaats,count(1) as 'count' from tri(?),oprs,nums where oprs.naam=a and oprs.id==nums.ligtAanRef group by oprs.id order by 4 desc limit 10", {street});
   });
   */
+
+  svr.set_logger([](const auto& req, const auto&) {
+    fmt::print("Request: {} {}\n", req.path, req.params);
+  });
   
   int port = argc==1 ? 8080 : atoi(argv[1]);
   cout<<"Will listen on http://127.0.0.1:"<<port<< " using "<<std::thread::hardware_concurrency() << " threads\n";
